3813a3c8f0f6bb65df9b04bef621ab4e2be5f9b0
Plastic Digits <plasticdigits@protonamil.com>
2022-06-24 06:52:51 -0700

    fix medium link

--- constants/links.js
+++ constants/links.js
@@ -4,7 +4,7 @@ export const LINK_TELEGRAM_ANN = "https://t.me/CZodiacANN";
 export const LINK_DISCORD      = "https://discord.gg/QDyTJccdE9";
 export const LINK_GITHUB       = "https://github.com/chinese-zodiac/czodiac";
 export const LINK_WHITEPAPER   = "https://czodiac.gitbook.io/czodiac-litepapper";
-export const LINK_MEDIUM       = "https://czodiacs.medium.com";
+export const LINK_MEDIUM       = "https://czodiac.medium.com";
 
 export const LINK_CZFARM       = "https://cz.farm";
 export const LINK_CZCASH       = "https://cz.cash";
