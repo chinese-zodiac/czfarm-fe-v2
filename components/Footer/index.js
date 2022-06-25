bd08ddf6c9cf224d30f41bdbf0006935ac1422f7
Plastic Digits <plasticdigits@protonamil.com>
2022-06-25 02:05:52 -0700

    Add disclaimers and warnings

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -28,6 +28,41 @@ function Footer() {
         </a>
       </div>
     </div>
+    <p>
+      <b>IMPORTANT: Do not expect profits from other's efforts.</b><br/>
+      <small>
+        Nothing on this site or on related channels should be considered a promise by anyone, including but not limited to the developers and promoters of this site, to perform work to generate profits for anyone including but not limited to the following: the users of this site; CZodiac community members; CZF holders; CZUSD holders; or anyone using any of the sites, smart contracts, social media channels, and any other media or tech related to CZF and CZodiac or any of the community members. Czodiac, CZF, cz.farm, and related technologies plus media are all experimental and must be used according to your personal financial situation and risk profile. There are no guarantees of profits, but the smart contracts are guaranteed to perform as written on the BSC blockchain.
+      </small>
+    </p>
+    <br/>
+    <p>
+      <b>WARNING: You may incur a tax liability.</b><br/>
+      <small>
+      Please consult your tax adviser before using CZ.FARM, CZodiac products, CZodiac tokens, other dapps linked by this site. Just like for every blockchain dapp, there may be complex taxation issues depending on your jurisdiction.
+      </small>
+    </p>
+    <br/>
+    <p>
+      <b>WARNING: You are using experimental technology.</b><br/>
+      <small>
+      All CZodiac products are experimental technology which may fail in unexpected ways from causes such as hacks, tokenomic design, software errors, or other unexpected issues that can not be anticipated as with any other experimental technology. Do not trust CZodiac dapps or other products with more funds than you can afford to use. Use CZodiac at your own risk, just as you would with any other innovative blockchain dapp.
+      </small>
+    </p>
+    <br/>
+    <p>
+      <b>DISCLAIMER: Use at your own risk.</b><br/>
+      <small>
+      All information provided by CZ.FARM and other CZodiac products, tokens, and dapps has no guarantee of completeness, accuracy, timeliness or of the results obtained from the use of information and/or CZodiac products, and without warranty of any kind, express or implied, including but not limited to warranties of performance, merchantability and fitness for a particular purpose.<br/><br/>
+      CZodiac and its members, team, developers, affiliates, associates, token holders, or other individuals or organizations involved in CZodiac products, dapps, or other services will not be liable to you or anyone else for any decision made or action taken in reliance on the information given by CZ.FARM or other CZodiac dapps or services for any consequential, special or similar damages, even if advised of the possibility of such damages.
+      </small>
+    </p>
+    <br/>
+    <p>
+      <b>CONTACT</b><br/>
+      <small>
+        For questions about the above notices, warnings, and/or disclaimers you may contact via email "team @ czodiac . com".
+      </small>
+    </p>
   </footer>);
 }
 
