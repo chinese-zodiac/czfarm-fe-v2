0c421d958b593fea8465015a1cc312fa204f89a4
Plastic Digits <plasticdigits@protonmail.com>
2023-04-14 06:22:17 -0700

    Update terms of use

--- constants/links.js
+++ constants/links.js
@@ -1,13 +1,17 @@
-export const LINK_TWITTER      = "https://twitter.com/zodiacs_c";
-export const LINK_TELEGRAM     = "https://t.me/CZodiacofficial";
+export const LINK_TWITTER = "https://twitter.com/zodiacs_c";
+export const LINK_TELEGRAM = "https://t.me/CZodiacofficial";
 export const LINK_TELEGRAM_ANN = "https://t.me/CZodiacANN";
-export const LINK_DISCORD      = "https://discord.gg/QDyTJccdE9";
-export const LINK_GITHUB       = "https://github.com/chinese-zodiac/czodiac";
-export const LINK_WHITEPAPER   = "https://czodiac.gitbook.io/czodiac-litepapper";
-export const LINK_MEDIUM       = "https://czodiac.medium.com";
+export const LINK_DISCORD = "https://discord.gg/QDyTJccdE9";
+export const LINK_GITHUB = "https://github.com/chinese-zodiac/czodiac";
+export const LINK_WHITEPAPER = "https://czodiac.gitbook.io/czodiac-litepapper";
+export const LINK_MEDIUM = "https://czodiac.medium.com";
 
-export const LINK_CZFARM       = "https://cz.farm";
-export const LINK_CZCASH       = "https://cz.cash";
-export const LINK_CZODIAC      = "https://czodiac.com";
-export const LINK_NUMIS        = "https://numis.cz.cash";
-export const LINK_OBR          = "https://bad.rabbitcatch.com";
+export const LINK_CZFARM = "https://cz.farm";
+export const LINK_CZCASH = "https://cz.cash";
+export const LINK_CZODIAC = "https://czodiac.com";
+export const LINK_NUMIS = "https://numis.cz.cash";
+export const LINK_OBR = "https://bad.rabbitcatch.com";
+
+
+export const LINK_TERMS_OF_USE = "https://czodiac.gitbook.io/czodiac-litepaper/terms-of-use";
+export const LINK_PRIVACY_POLICY = "https://czodiac.gitbook.io/czodiac-litepaper/privacy-policy";
