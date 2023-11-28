813c22324a15817e7a1cd130d7d3264f7c7c7aad
Plastic Digits <plasticdigits@protonmail.com>
2023-11-28 16:06:21 +0900

    Update rpc

--- pages/_app.js
+++ pages/_app.js
@@ -1,63 +1,63 @@
+import { BSC, DAppProvider } from '@usedapp/core';
 import Head from 'next/head';
+import { withRouter } from 'next/router';
 import React from 'react';
-import Router, { withRouter } from 'next/router';
-import { DAppProvider, BSC} from '@usedapp/core'
-import OpenGraphImg from '../public/static/assets/opengraph.jpg';
-import Favicon from '../public/static/assets/logo192.png';
-import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { CZFarmProvider } from '../contexts/CZFarmContext';
 import '../public/static/assets/fonts/stylesheet.css';
+import Favicon from '../public/static/assets/logo192.png';
+import OpenGraphImg from '../public/static/assets/opengraph.jpg';
 import '../styles/styles.scss';
-import {CZFarmProvider} from '../contexts/CZFarmContext';
 
 const config = {
   readOnlyChainId: BSC.chainId,
   readOnlyUrls: {
-    [BSC.chainId]: 'https://bscrpc.com'
+    [BSC.chainId]: 'https://bsc-dataseed1.binance.org/'
   },
-  networks:[BSC]
+  networks: [BSC]
 }
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <DAppProvider config={config}>
-        <Head>
-          <title>CZ.Farm | Stablecoin Yield And High Yield Farms And Pools</title>
-          <meta name="description" content= "Earn the best BSC tokens with stablecoin yield and defi high yield farms and pools." />
-          <meta name="robots" content= "index, follow"></meta>
-          <meta property="og:locale" content="en_EN"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link
-            rel="shortcut icon"
-            type="image/png"
-            href={Favicon}
-          />
-            
-          <meta property="og:title" content="CZ.Farm | Stablecoin Yield And High Yield Farms And Pools" />
-          <meta property="og:site_name" content="CZ.Farm" />
-          <meta property="og:url" content="https://v2.cz.farm" />
-          <meta property="og:description" content="Earn the best BSC tokens with stablecoin yield and defi high yield farms and pools." />
-          <meta property="og:type" content="article" />
-          <meta property="og:image" content={"https://v2.cz.farm"+OpenGraphImg} />
-          <meta property="og:image:width" content="1200" /> 
-          <meta property="og:image:height" content="630" />
+      <Head>
+        <title>CZ.Farm | Stablecoin Yield And High Yield Farms And Pools</title>
+        <meta name="description" content="Earn the best BSC tokens with stablecoin yield and defi high yield farms and pools." />
+        <meta name="robots" content="index, follow"></meta>
+        <meta property="og:locale" content="en_EN" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href={Favicon}
+        />
 
-          <meta name="twitter:card" content="summary_large_image"/>
-          <meta name="twitter:site" content="https://v2.cz.farm" />
-          <meta name="twitter:title" content="CZ.Farm | Stablecoin Yield And High Yield Farms And Pools"/>
-          <meta name="twitter:image" content={"https://v2.cz.farm"+OpenGraphImg} />
-          <meta name="twitter:image:width" content="1200"/>
-          <meta name="twitter:image:height" content="630"/>
-          <meta name="twitter:description" content="Earn the best BSC tokens with stablecoin yield and defi high yield farms and pools."/>
+        <meta property="og:title" content="CZ.Farm | Stablecoin Yield And High Yield Farms And Pools" />
+        <meta property="og:site_name" content="CZ.Farm" />
+        <meta property="og:url" content="https://v2.cz.farm" />
+        <meta property="og:description" content="Earn the best BSC tokens with stablecoin yield and defi high yield farms and pools." />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content={"https://v2.cz.farm" + OpenGraphImg} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="https://v2.cz.farm" />
+        <meta name="twitter:title" content="CZ.Farm | Stablecoin Yield And High Yield Farms And Pools" />
+        <meta name="twitter:image" content={"https://v2.cz.farm" + OpenGraphImg} />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
+        <meta name="twitter:description" content="Earn the best BSC tokens with stablecoin yield and defi high yield farms and pools." />
 
 
-        </Head>
-        <CZFarmProvider>
-          <Header />
-          <Component {...pageProps} />
-          <Footer />
-        </CZFarmProvider>        
+      </Head>
+      <CZFarmProvider>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </CZFarmProvider>
     </DAppProvider>
   );
 }
